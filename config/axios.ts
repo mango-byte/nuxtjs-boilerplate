faabe1647e45dca9e730568b78e3de1e3682c0cc
Touch <s.chantouch@mango-byte.com>
2021-12-24 18:28:13 +0700

    feat: :rocket: add playwright for e2e test

--- config/axios.ts
+++ config/axios.ts
@@ -1,4 +1,8 @@
+const proxy = process.env.CI ? true : process.env.PROXY === 'true'
+export const isDev = process.env.NODE_ENV !== 'production'
 export const axios = {
-  // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-  baseURL: '/',
+  proxy,
+  retry: true,
+  debug: isDev,
+  credentials: true,
 }
